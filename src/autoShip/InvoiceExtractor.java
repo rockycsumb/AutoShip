bd52cf058bcf99453cfbdf0787a21508c9024e08
rockycsumb <37380272+rockycsumb@users.noreply.github.com>
2018-09-25 18:01:45 -0700

    delivery and po number made

--- src/autoShip/InvoiceExtractor.java
+++ src/autoShip/InvoiceExtractor.java
@@ -1,16 +1,42 @@
 package autoShip;
+import java.util.Scanner;
 
 public class InvoiceExtractor
 {   
    boolean isValidDeliveryNumber = false;
+   public static InvoiceSeparator Separate = new InvoiceSeparator();
+   int deliveryNumber = 0;
+   
 
    public InvoiceExtractor()
    {
-      InvoiceSeparator Separate = new InvoiceSeparator();
-      testArrayList();
+      Scanner kbd = new Scanner(System.in);
+      String userChoice = "n";
       
+      //testArrayList();
       
-     
+      int sizeIndex;
+      int invoiceIndex;
+
+      for(sizeIndex = 0; sizeIndex < Separate.startIndexArray.size(); sizeIndex++)
+      {
+         
+         for(invoiceIndex = Separate.startIndexArray.get(sizeIndex); invoiceIndex < Separate.endIndexArray.get(sizeIndex); invoiceIndex ++)
+         {
+
+           
+            deliveryInfoCheck(ProcessInvoice.lines[invoiceIndex]);
+            if(isValidDeliveryNumber)
+            {
+               setPONumber(ProcessInvoice.lines[invoiceIndex]);
+            }
+            
+         }
+         isValidDeliveryNumber = false;
+         System.out.print("Next Invoice? ");
+         userChoice = kbd.next();       
+      }
+      
    }
       
    
@@ -24,6 +50,8 @@ public class InvoiceExtractor
          if (deliveryInfo.substring(0, 1).equals("8"))
          {
             //System.out.println("from delivery check " + deliveryInfo.substring(0, 9));
+            deliveryNumber = Integer.valueOf(deliveryInfo.substring(0,9));
+            System.out.println("from delivery check " + deliveryNumber);
             isValidDeliveryNumber = true;
             return true;
          }      
@@ -32,14 +60,30 @@ public class InvoiceExtractor
    }
    
    
+   boolean setPONumber(String poNumber)
+   {
+      int poNumberBegins, poNumberEnds = 0;
+      
+      if (poNumber.length() <= 26 && 
+            poNumber.length() > 12 &&
+            poNumber.substring(0,1).equals("8"))
+      {
+         poNumberBegins = poNumber.indexOf(" ");
+         poNumberEnds = poNumber.indexOf(" ", poNumberBegins + 1);
+         poNumber = poNumber.substring(poNumberBegins, poNumberEnds);
+         System.out.println("Po Number  " + poNumber);
+              
+      }
+      return false;
+   }
+   
    public void testArrayList() 
    {
-      InvoiceSeparator Separate = new InvoiceSeparator();
       int row;
       System.out.println("From Invoice Extractor");
       for(row = 0; row < Separate.startIndexArray.size(); row++)
       {
-       System.out.println("here is the start " + Separate.startIndexArray.get(row) + " here is the end " + Separate.endIndexArray.get(row));  
+       System.out.println("here is the start from invoice extract " + Separate.startIndexArray.get(row) + " here is the end " + Separate.endIndexArray.get(row));  
       }  
    }
 }
